e9803ede2da5d7b21b228328c15f84edf3529301
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-07-09 15:33:12 -0400

    Day 86 - "How to use gestures in SwiftUI" part 2

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,23 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentAmount: CGFloat = 0
-    @State private var finalAmount: CGFloat = 1
+    // how far the circle has been dragged
+    @State private var offset = CGSize.zero
+
+    // whether it is currently being dragged or not
+    @State private var isDragging = false
 
     var body: some View {
-        Text("Hello, World!")
-            .scaleEffect(finalAmount + currentAmount)
-            .gesture(
-                MagnificationGesture()
-                    .onChanged { amount in
-                        self.currentAmount = amount - 1
-                    }
-                    .onEnded { amount in
-                        //self.finalAmount = 1
-                        self.finalAmount += self.currentAmount
-                        self.currentAmount = 0
-                    }
-            )
+        // a drag gesture that updates offset and isDragging as it moves around
+        let dragGesture = DragGesture()
+            .onChanged { value in self.offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    self.offset = .zero
+                    self.isDragging = false
+                }
+            }
+
+        // a long press gesture that enables isDragging
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    self.isDragging = true
+                }
+            }
+
+        // a combined gesture that forces the user to long press then drag
+        let combined = pressGesture.sequenced(before: dragGesture)
+
+        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
+        return Circle()
+            .fill(Color.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
     }
 }
 
