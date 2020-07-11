80dd3512992c9f6da19f6b648779e4800e32df07
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-07-11 17:18:56 -0400

    Day 87 - "Triggering events repeatedly using a timer"

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,58 +8,20 @@
 
 import SwiftUI
 
-struct Shaper : View {
-    let isCircle : Bool
-    var body: some View {
-        return ZStack {
-            if isCircle {
-                Capsule()
-            } else {
-                Rectangle()
-            }
-        }
-    }
-}
-
 struct ContentView: View {
-    @State private var isCircle = false
-    // how far the circle has been dragged
-    @State private var offset = CGSize.zero
-
-    // whether it is currently being dragged or not
-    @State private var isDragging = false
-
+    @State private var counter = 0
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        // a drag gesture that updates offset and isDragging as it moves around
-        let dragGesture = DragGesture()
-            .onChanged { value in self.offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    self.offset = .zero
-                    self.isDragging = false
-                    self.isCircle.toggle()
+        Text("hello world ")
+            .onReceive(timer) { time in
+                if self.counter == 5 {
+                    self.timer.upstream.connect().cancel()
+                } else {
+                    print("the time is \(time)")
                 }
-            }
-
-        // a long press gesture that enables isDragging
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    self.isDragging = true
-                }
-            }
-
-        // a combined gesture that forces the user to long press then drag
-        let combined = pressGesture.sequenced(before: dragGesture)
-
-        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
-        return Shaper(isCircle: isCircle)
-            //.fill(Color.red)
-            .foregroundColor(.blue)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+                self.counter += 1
+        }
     }
 }
 
